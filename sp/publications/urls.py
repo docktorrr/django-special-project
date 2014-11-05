ab5760e82fe8e0399c7436bf2b559bcaeb71171e
docktorrr <nikolay.kovalenko@gmail.com>
2014-11-05 18:36:47 +0300

    Recipe filtering + default quiz templates

--- sp/publications/urls.py
+++ sp/publications/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^(\d+)/$', 'sp.publications.views.article', name='article'),
+    url(r'^recipe/(\d+)/$', 'sp.publications.views.recipe', name='recipe'),
+    url(r'^recipes/$', 'sp.publications.views.recipes', name='recipes'),
 )
--- sp/publications/views.py
+++ sp/publications/views.py
@@ -1,9 +1,33 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from sp.publications.models import *
+from django.conf import settings
+from sp.publications.models import Article, Recipe, Tag
 
 
 def article(request, id):
     article = get_object_or_404(Article, id=id)
-    return render_to_response('publications/article.html', {'article': article}, context_instance=RequestContext(request))
+    return render_to_response('%s/%s' % (settings.TEMPLATE_THEME, 'publications/article.html'), {'article': article}, context_instance=RequestContext(request))
+
+def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render_to_response('%s/%s' % (settings.TEMPLATE_THEME, 'publications/recipe.html'), {'recipe': recipe}, context_instance=RequestContext(request))
+    
+def recipes(request):
+    if request.method == 'POST':
+        dict = request.POST
+    else:
+        dict = request.GET
+    recipes = Recipe.objects.all()
+    if 'tag' in dict:
+        try:
+            tag = Tag.objects.get(name=dict['tag'])
+            recipes = recipes.filter(tags=tag)
+        except Tag.DoesNotExist:
+            recipes = recipes.none()
+    if 'calories' in dict:
+        recipes = recipes.filter(calories=dict['calories'])
+    if 'time' in dict:
+        recipes = recipes.filter(cooking_time=dict['time'])
+    
+    return render_to_response('%s/%s' % (settings.TEMPLATE_THEME, 'publications/recipes.html'), {'recipes': recipes}, context_instance=RequestContext(request))
