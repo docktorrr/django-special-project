93bde0c5d8cc9e659b4456375e83d5294aeb184b
unknown <nkovalenko@PCN-01.milanor.ru>
2015-08-04 16:29:24 +0300

    twitter profile link fix

--- sp/contest/admin.py
+++ sp/contest/admin.py
@@ -44,7 +44,7 @@ class WorkAdmin(admin.ModelAdmin):
             if sa.provider == 'vk-oauth':
                 links.append('<a href="http://www.vk.com/id%s" target="_blank">http://www.vk.com/id%s</a>' % (sa.uid, sa.uid))
             if sa.provider == 'twitter':
-                links.append('<a href="https://twitter.com/account/redirect_by_id?id=%s" target="_blank">https://twitter.com/account/redirect_by_id?id=%s</a>' % (sa.uid, sa.uid))
+                links.append('<a href="https://twitter.com/intent/user/?user_id=%s" target="_blank">https://twitter.com/intent/user/?user_id=%s</a>' % (sa.uid, sa.uid))
             if sa.provider == 'odnoklassniki':
                 links.append('<a href="http://www.odnoklassniki.ru/profile/%s" target="_blank">http://www.odnoklassniki.ru/profile/%s</a>' % (sa.uid, sa.uid))
             if sa.provider == 'google-oauth2':
